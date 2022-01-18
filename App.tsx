0c998e100ffbdff14ccd3f5bef9cc95c8a4eefbe
Christopher Alves <christopher.alves@portalsolar.com.br>
2022-01-18 10:03:31 -0300

    Add Tab Navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { Home } from './src/pages/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" />
-      <Home />
-    </>
+      <AppRoutes />
+    </NavigationContainer>
   );
 };
 
